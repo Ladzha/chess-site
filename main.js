290657db9014becf061dc34cb9518623c89c59a0
Alla <allanudelman@gmail.com>
2024-02-20 00:16:24 +0400

    mobile

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-const cardsContainer = document.querySelector('.cards-container');
+const slider = document.querySelector('.cards');
+const cards = document.querySelectorAll('.card')
+
 const btnPrev = document.querySelector('.carousel-btn-left');
 const btnNext = document.querySelector('.carousel-btn-right');
 
 btnPrev.addEventListener('click', swipePrev)
 btnNext.addEventListener('click', swipeNext)
 
+let currentIndex=0;
+const cardWidth = cards[0].offsetWidth;
+
+function swipeSlider(index){
+  slider.style.transform = `translateX(${-cardWidth * index}px)`
+}
+
 function swipePrev(){
-  console.log('swipePrev');
+  if(currentIndex <= 0){
+    currentIndex = cards.length-1;
+  }else{
+    currentIndex--;
+  }
+  swipeSlider(currentIndex)
 }
 
 function swipeNext(){
-  console.log('swipeNext');
+  if(currentIndex >= cards.length-1){
+    currentIndex = 0
+  }
+  else{
+    currentIndex++;
+  }
+  swipeSlider(currentIndex);
 }
+
+setInterval(()=>swipeSlider(currentIndex), 1000)
